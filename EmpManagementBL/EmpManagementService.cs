036f3d57e6f85d45753086e88a09001b545ba5f9
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-10-12 20:12:44 +0530

    [SAMPADA] ADD : Employee Management Business Layer

---
+++ EmpManagementBL/EmpManagementService.cs
@@ -0,0 +1,53 @@
+﻿using EmpManagementBL.implementation;
+using EmpManagementML;
+using EmpManagementRL;
+using EmpManagementRL.implementation;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpManagementBL
+{
+    public class EmpManagementService : IEmpManagementService
+    {
+
+        private readonly IEmpManagementRepository empManagementRepository;
+        public EmpManagementService(IEmpManagementRepository empManagementRepository)
+        {
+            this.empManagementRepository = empManagementRepository;
+        }
+
+        public EmpManagementModel AddEmployee(EmpManagementModel empManagementModel)
+        {
+            return this.empManagementRepository.AddEmployee(empManagementModel);
+        }
+
+        public bool DeleteEmployee(string EmpID)
+        {
+            return this.empManagementRepository.DeleteEmployee(EmpID);
+        }
+
+        public EmpManagementModel UpdateEmployee(EmpManagementModel empManagementModel)
+        {
+            return this.empManagementRepository.UpdateEmployee(empManagementModel);
+        }
+
+        public DataSet GetAllEmployee()
+        {
+            return this.empManagementRepository.GetAllEmployee();
+        }
+
+        public EmpManagementModel GetEmployeeOnBasisfEmailID(string EmailID)
+        {
+            return this.empManagementRepository.GetEmployeeOnBasisfEmailID(EmailID);
+        }
+
+        public EmpManagementModel GetEmployeeOnBasisfEmpID(int EmpID)
+        {
+            return this.empManagementRepository.GetEmployeeOnBasisfEmpID(EmpID);
+        }
+    }
+}
