bf1499f3d6d2d273db590f61d89124dc8e30d874
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-10-12 20:10:32 +0530

    [SAMPADA] ADD : Employee Management Model Layer

---
+++ EMpManagementML/EmpManagementModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpManagementML
+{
+    public class EmpManagementModel
+    {
+        public int EmpID { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailID { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public int DepartmentID { get; set; }
+    }
+}
