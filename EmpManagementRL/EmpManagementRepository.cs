c2823fc391d124b3e7a28c14b96fa90169b67dc9
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-10-12 20:14:47 +0530

    [SAMPADA] ADD : Employee Management Repository Layer

---
+++ EmpManagementRL/EmpManagementRepository.cs
@@ -0,0 +1,212 @@
+﻿using EmpManagementML;
+using EmpManagementRL.implementation;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpManagementRL
+{
+    public class EmpManagementRepository : IEmpManagementRepository
+    {
+        static string connect = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+        SqlConnection connection = new SqlConnection(connect);
+        //SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=EmployeeManagement;Integrated Security=True");
+        public EmpManagementModel AddEmployee(EmpManagementModel empManagementModel)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("SPAddEmployeeData", this.connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@FirstName", empManagementModel.FirstName);
+                    command.Parameters.AddWithValue("@LastName", empManagementModel.LastName);
+                    command.Parameters.AddWithValue("@EmailID", empManagementModel.EmailID);
+                    command.Parameters.AddWithValue("@PhoneNumber", empManagementModel.PhoneNumber);
+                    command.Parameters.AddWithValue("@DepartmentID", empManagementModel.DepartmentID);
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    if (result > 0)
+                    {
+                        return this.GetEmployeeOnBasisfEmailID(empManagementModel.EmailID);
+                    }
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+        public EmpManagementModel UpdateEmployee(EmpManagementModel empManagementModel)
+        {
+            try
+            {
+                using(this.connection)
+                {
+                    SqlCommand command = new SqlCommand("SPUpdateEmployeeData",this.connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmpID",empManagementModel.EmpID);
+                    command.Parameters.AddWithValue("@FirstName", empManagementModel.FirstName);
+                    command.Parameters.AddWithValue("@LastName", empManagementModel.LastName);
+                    command.Parameters.AddWithValue("@EmailID", empManagementModel.EmailID);
+                    command.Parameters.AddWithValue("@PhoneNumber", empManagementModel.PhoneNumber);
+                    command.Parameters.AddWithValue("@DepartmentID", empManagementModel.DepartmentID);
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+                    if(result != 0)
+                    {
+                        return this.GetEmployeeOnBasisfEmpID(empManagementModel.EmpID);
+                    }
+                    return null;
+                }
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+        public bool DeleteEmployee(string EmpID)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("SPDeleteEmployeeData", this.connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmpID", Convert.ToInt32(EmpID));
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    if(result > 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+        public DataSet GetAllEmployee()
+        {
+            try
+            {
+                DataSet dataset = new DataSet();
+                using(this.connection)
+                {
+                    this.connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter("SPGetEmployeeList", this.connection);
+                    adapter.Fill(dataset);
+                    this.connection.Close();
+                    return dataset;
+                }
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                //this.connection.Close();
+            }
+        }
+
+        public EmpManagementModel GetEmployeeOnBasisfEmailID(string EmailID)
+        {
+            try {
+                using (this.connection)
+                {
+                    EmpManagementModel empManagementModel = new EmpManagementModel();
+                    SqlCommand command = new SqlCommand("SpGetEmployeeDetailsOnParticularEmailID", this.connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmailID", EmailID);
+                    this.connection.Open();
+                    SqlDataReader dataReader = command.ExecuteReader();
+                    if (dataReader.HasRows)
+                    {
+                        while (dataReader.Read())
+                        {
+                            empManagementModel.EmpID = Convert.ToInt32(dataReader["EmpID"]);
+                            empManagementModel.FirstName = dataReader["FirstName"].ToString();
+                            empManagementModel.LastName = dataReader["LastName"].ToString();
+                            empManagementModel.EmailID = dataReader["EmailID"].ToString();
+                            empManagementModel.PhoneNumber = Convert.ToInt32(dataReader["PhoneNumber"]);
+                            empManagementModel.DepartmentID = Convert.ToInt32(dataReader["DepartmentID"]);
+                        }
+                        return empManagementModel;
+                    }
+                    return null;
+                }
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+        public EmpManagementModel GetEmployeeOnBasisfEmpID(int EmpID)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    EmpManagementModel empManagementModel = new EmpManagementModel();
+                    SqlCommand command = new SqlCommand("SpGetEmployeeDetailsOnParticularEmpID", this.connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmpID", EmpID);
+                    this.connection.Open();
+                    SqlDataReader dataReader = command.ExecuteReader();
+                    if (dataReader.HasRows)
+                    {
+                        while (dataReader.Read())
+                        {
+                            empManagementModel.EmpID = Convert.ToInt32(dataReader["EmpID"]);
+                            empManagementModel.FirstName = dataReader["FirstName"].ToString();
+                            empManagementModel.LastName = dataReader["LastName"].ToString();
+                            empManagementModel.EmailID = dataReader["EmailID"].ToString();
+                            empManagementModel.PhoneNumber = Convert.ToInt32(dataReader["PhoneNumber"]);
+                            empManagementModel.DepartmentID = Convert.ToInt32(dataReader["DepartmentID"]);
+                        }
+                        return empManagementModel;
+                    }
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+    }
+}
---
+++ EmpManagementRL/interface/IEmpManagementRepository.cs
@@ -0,0 +1,25 @@
+﻿using EmpManagementML;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpManagementRL.implementation
+{
+    public interface IEmpManagementRepository
+    {
+        EmpManagementModel AddEmployee(EmpManagementModel empManagementModel);
+
+        EmpManagementModel UpdateEmployee(EmpManagementModel empManagementModel);
+
+        bool DeleteEmployee(string EmpID);
+
+        DataSet GetAllEmployee();
+
+        EmpManagementModel GetEmployeeOnBasisfEmailID(string EmailID);
+
+        EmpManagementModel GetEmployeeOnBasisfEmpID(int EmpID);
+    }
+}
